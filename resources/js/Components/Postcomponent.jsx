23ec9504b5eb4663051b380854dbc65ca3bf39e5
yahya akli <akli.yahya05@gmail.com>
2024-08-07 20:00:43 +0100

    yahya

--- resources/js/Components/Postcomponent.jsx
+++ resources/js/Components/Postcomponent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import UserAvatar from './UserAvatar';
+import { formatPostDate } from '@/helpers';
 
 export default function PostComponent({ post, user }) {
     const carouselRef = useRef(null);
@@ -57,13 +58,16 @@ export default function PostComponent({ post, user }) {
 
     return (
         <div className='p-6 my-12 mx-4 bg-gray-100 rounded-xl'>
-            <div className='flex items-center gap-2 text-lg py-2'>
-                <UserAvatar user={user} /> {user.username}
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center gap-2 text-lg py-2'>
+                    <UserAvatar user={user} /> {user.username}
+                </div>
+                <span>{formatPostDate(post.created_at)}</span>
             </div>
-            <div className='py-2 text-2xl'>
+            <div className='py-2 text-2xl mt-5'>
                 {post.title}
             </div>
-            <div className='py-2 text-md'>
+            <div className='py-2 text-md mb-6'>
                 {post.description}
             </div>
             {post.attachments.length > 0 && (
@@ -84,20 +88,20 @@ export default function PostComponent({ post, user }) {
                             <>
                                 <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
                                     {post.attachments.map((_, index) => (
-                                        <button key={index} type="button" className={`w-3 h-3 rounded-full ${index === 0 ? 'bg-blue-600' : 'bg-white'}`} aria-current={index === 0 ? "true" : "false"} aria-label={`Slide ${index + 1}`} data-carousel-slide-to={index}></button>
+                                        <button key={index} type="button" className={`w-3 h-3 rounded-full border-1 border ${index === 0 ? 'bg-blue-600' : 'bg-white'}`} aria-current={index === 0 ? "true" : "false"} aria-label={`Slide ${index + 1}`} data-carousel-slide-to={index}></button>
                                     ))}
                                 </div>
                                 <button type="button" className="absolute top-1/2 left-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none -translate-y-1/2" data-carousel-prev>
-                                    <span className="text-white dark:text-gray-400 dark:hover:text-white transition-all inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-100 group-hover:bg-white/50 dark:group-hover:bg-gray-300 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                        <svg className="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <span className="text-white dark:text-gray-400 dark:hover:text-black transition-all inline-flex items-center justify-center sm:w-10 sm:h-10 h-5 w-5 rounded-full bg-white/30 dark:bg-gray-100/50 group-hover:bg-white/50 dark:group-hover:bg-gray-300/80 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                        <svg className="sm:w-4 sm:h-4 w-2 h-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
                                         </svg>
                                         <span className="sr-only">Previous</span>
                                     </span>
                                 </button>
                                 <button type="button" className="absolute top-1/2 right-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none -translate-y-1/2" data-carousel-next>
-                                    <span className="text-white dark:text-gray-400 dark:hover:text-white transition-all inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-100 group-hover:bg-white/50 dark:group-hover:bg-gray-300 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                        <svg className="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <span className="text-white dark:text-gray-400 dark:hover:text-black transition-all inline-flex items-center justify-center sm:w-10 sm:h-10 h-5 w-5 rounded-full bg-white/30 dark:bg-gray-100/50 group-hover:bg-white/50 dark:group-hover:bg-gray-300/80 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                        <svg className="sm:w-4 sm:h-4 w-2 h-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
                                         </svg>
                                         <span className="sr-only">Next</span>
